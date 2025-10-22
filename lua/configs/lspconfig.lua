6c25cf4c8e327f8123157410f2c2fc7c823031bf
Bydanov Ilya <bydanav@gmail.com>
2025-10-23 01:14:11 +0300

    Обновил конфиг lspconfig исходя из нового синтаксиса

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -6,8 +6,7 @@ local on_attach = configs.on_attach
 local on_init = configs.on_init
 local capabilities = configs.capabilities
 
--- Импортируем lspconfig, чтобы настраивать различные LSP-серверы
-local lspconfig = require "lspconfig"
+-- Используем новый API vim.lsp.config вместо устаревшего require "lspconfig"
 
 -- Если требуется стандартная конфигурация для серверов, добавляем их в таблицу servers
 local servers = {
@@ -37,113 +36,147 @@ local function organize_imports()
   vim.lsp.buf.execute_command(params)
 end
 
--- Настраиваем каждый сервер из списка servers
+-- Настраиваем каждый сервер из списка servers с базовой конфигурацией
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  vim.lsp.config(lsp, {
     on_attach = on_attach,       -- Привязываем стандартные действия при подключении сервера
     capabilities = capabilities, -- Задаем возможности для клиента
-    commands = {
-      -- Добавляем команду OrganizeImports для TypeScript, чтобы организовывать импорты
-      OrganizeImports = {
-        organize_imports,
-        description = "Организовать импорты",
+  })
+end
+
+-- Отдельные настройки для серверов с особыми требованиями
+
+-- Настройка сервера TypeScript с командой OrganizeImports
+vim.lsp.config('ts_ls', {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  commands = {
+    -- Добавляем команду OrganizeImports для TypeScript, чтобы организовывать импорты
+    OrganizeImports = {
+      organize_imports,
+      description = "Организовать импорты",
+    },
+  },
+})
+
+-- Настройка сервера Go с дополнительными настройками
+vim.lsp.config('gopls', {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    gopls = {
+      completeUnimported = true, -- Автодополнение для неимпортированных пакетов
+      usePlaceholders = true,    -- Использовать заполнители для аргументов
+      analyses = {
+        unusedparams = true,     -- Включить анализ на неиспользуемые параметры
       },
     },
-    settings = {
-      -- Настройки для Go-сервера (gopls)
-      gopls = {
-        completeUnimported = true, -- Автодополнение для неимпортированных пакетов
-        usePlaceholders = true,    -- Использовать заполнители для аргументов
-        analyses = {
-          unusedparams = true,     -- Включить анализ на неиспользуемые параметры
-        },
-      },
+  },
+})
+
+-- Настройка сервера Prisma LSP
+vim.lsp.config('prismals', {
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+-- Настройка сервера Volar для работы с Vue и TypeScript файлами
+vim.lsp.config('volar', {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+  init_options = {
+    vue = {
+      hybridMode = false, -- Отключаем гибридный режим для Volar
     },
-  }
+  },
+})
 
-  -- Отдельная настройка для сервера Prisma LSP
-  lspconfig.prismals.setup {}
-
-  -- Настройка сервера Volar для работы с Vue и TypeScript файлами
-  lspconfig.volar.setup {
-    on_attach = on_attach,
-    filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-    init_options = {
-      vue = {
-        hybridMode = false, -- Отключаем гибридный режим для Volar
-      },
+-- Настройка сервера Ansible LSP
+vim.lsp.config('ansiblels', {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "ansible" },
+  settings = {
+    ansible = {
+      path = "ansible", -- При необходимости отрегулируйте путь
+      lint = true,
     },
-  }
+  },
+})
 
-  -- Настройка сервера Ansible LSP
-  lspconfig.ansiblels.setup {
-    on_attach = on_attach,       -- Ваша функция on_attach
-    filetypes = { "ansible" },
-    capabilities = capabilities, -- Ваши возможности
-    settings = {
-      ansible = {
-        path = "ansible", -- При необходимости отрегулируйте путь
-        lint = true,
-      },
-    },
-  }
-
-  -- Настройка сервера Helm LSP
-  lspconfig.helm_ls.setup {
-    settings = {
-      ['helm-ls'] = {
-        yamlls = {
-          path = "yaml-language-server",
-        }
+-- Настройка сервера Helm LSP
+vim.lsp.config('helm_ls', {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    ['helm-ls'] = {
+      yamlls = {
+        path = "yaml-language-server",
       }
     }
   }
+})
 
-  -- Настройка сервера YAML LSP
-  lspconfig.yamlls.setup {}
+-- Настройка сервера YAML LSP
+vim.lsp.config('yamlls', {
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
 
-  -- Настройка сервера Docker LSP
-  lspconfig.dockerls.setup {}
+-- Настройка сервера Docker LSP
+vim.lsp.config('dockerls', {
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
 
-  -- Настройка сервера Docker Compose LSP
-  lspconfig.docker_compose_language_service.setup {
-    on_attach = on_attach,
-    -- cmd = { 'docker-compose-langserver', '--stdio' },
-    filetypes = { 'yaml.docker-compose' },
-    single_file_support = true,
-  }
+-- Настройка сервера Docker Compose LSP
+vim.lsp.config('docker_compose_language_service', {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  -- cmd = { 'docker-compose-langserver', '--stdio' },
+  filetypes = { 'yaml.docker-compose' },
+  single_file_support = true,
+})
 
-  -- Настройка сервера Bash LSP
-  lspconfig.bashls.setup {
-    on_attach = on_attach,
-    filetypes = { "sh", "zsh", "bash" },
-    cmd = { "bash-language-server", "start" },
-    settings = {
-      bashIde = {
-        diagnostics = {
-          enable = true,
-        },
+-- Настройка сервера Bash LSP
+vim.lsp.config('bashls', {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "sh", "zsh", "bash" },
+  cmd = { "bash-language-server", "start" },
+  settings = {
+    bashIde = {
+      diagnostics = {
+        enable = true,
       },
     },
-  }
+  },
+})
 
-  -- Настройка сервера Nginx LSP
-  lspconfig.nginx_language_server.setup {
-    on_attach = on_attach,
-    filetypes = { "nginx" },
-    cmd = { "nginx-language-server" },
-    settings = {
-      nginxIde = {
-        diagnostics = {
-          enable = true,
-        },
+-- Настройка сервера Nginx LSP
+vim.lsp.config('nginx_language_server', {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "nginx" },
+  cmd = { "nginx-language-server" },
+  settings = {
+    nginxIde = {
+      diagnostics = {
+        enable = true,
       },
     },
-  }
+  },
+})
 
-  -- Настройка сервера Pyright
-  lspconfig.pyright.setup {}
+-- Настройка сервера Pyright
+vim.lsp.config('pyright', {
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
 
-  -- Настройка сервера Terraform LSP
-  -- lspconfig.terraformls.setup {}
-end
+-- Настройка сервера Terraform LSP
+-- vim.lsp.config('terraformls', {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+-- })
